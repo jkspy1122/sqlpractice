bd297d56adba350547cc9e5ed6e928a5bdd0ec95
jkspytest <jkspy1122@gmail.com>
2023-01-27 03:50:31 +0800

    add load previouseTickers from Database

--- app.js
+++ app.js
@@ -16,14 +16,24 @@ const connection = mysql.createConnection({
 connection.connect();
 
 //simple query
+// connection.query(
+//     'SELECT * FROM airbnb.exchangeTickers', function (err, results, fields) {
+//         console.log(results); // results contains rows returned by server
+//         //console.log(fields); // fields contains extra meta data about results, if available
+//     });
+
+
+//load all symbols with listingStatus=trading into array "previouseTickers"
+const previouseTicker = [];
 connection.query(
     'SELECT * FROM airbnb.exchangeTickers', function (err, results, fields) {
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
+        for (let i = 0; i < results.length; i++) {
+            if (results[i].listingStatus === "trading") {
+                previouseTicker.push(results[i].symbol);
+            }
+        }
+        console.log(previouseTicker);
     });
-
-
-//create ticker table
 
 
 //disconnect
