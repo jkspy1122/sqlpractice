21679f678ba58532bee525b2795a609ee2082c4a
jkspytest <jkspy1122@gmail.com>
2023-01-27 17:47:35 +0800

    done - add new ticker into database. WIP - updating ticker status from database and pop delisted ticker from previousTicker

--- app.js
+++ app.js
@@ -4,37 +4,113 @@ require('dotenv').config()
 const mysql = require('mysql2');
 
 
-//create connection to database
-const connection = mysql.createConnection({
+//create and init connection pool
+const pool  = mysql.createPool({
     host: process.env.host,
     user: process.env.user,
     password : process.env.password,
-    database : process.env.database
+    database : process.env.database,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-//connect
-connection.connect();
+class apiCollection {
+    constructor(api) {
+        this.api = api;
+        this.jsonData = null;
+    }
+    
+    async getJson() {
+        this.jsonData = await (await fetch(this.api)).json();
+        // console.log(this.jsonData);
+        return this.jsonData;
+    }
+    
+    async saveKlinedata() {
+        if (this.jsonData === null) {
+            console.log("You need to get json data first by calling getJson()");
+            return;
+        }
+        // console.log(typeof(this.jsonData));
+        let finalData = this.jsonData.map(d => {
+            return {time:d[0]/1000,open:parseFloat(d[1]),high:parseFloat(d[2]),low:parseFloat(d[3]),close:parseFloat(d[4])}
+          });
+        // console.log(finalData);
+    }
 
-//simple query
-// connection.query(
-//     'SELECT * FROM airbnb.exchangeTickers', function (err, results, fields) {
-//         console.log(results); // results contains rows returned by server
-//         //console.log(fields); // fields contains extra meta data about results, if available
-//     });
-
-
-//load all symbols with listingStatus=trading into array "previouseTickers"
-const previouseTicker = [];
-connection.query(
-    'SELECT * FROM airbnb.exchangeTickers', function (err, results, fields) {
-        for (let i = 0; i < results.length; i++) {
-            if (results[i].listingStatus === "trading") {
-                previouseTicker.push(results[i].symbol);
+    //check exchange info for latest ticker and save to var
+    async saveExchangeInfo() {
+        if (this.jsonData === null) {
+            console.log("You need to get json data first by calling getJson()");
+            return;
+        }
+        //conpare every symbol with previousTickers and insert new tickers into DATABASE and previousTickers
+        for (var i = 0 ; i < this.jsonData.symbols.length; i++) {
+            if (this.jsonData.symbols[i].contractType === "PERPETUAL" && this.jsonData.symbols[i].status === "TRADING" && !previousTickers.includes(this.jsonData.symbols[i].symbol)) {
+                previousTickers.push(this.jsonData.symbols[i].symbol);
+                var symbol = this.jsonData.symbols[i].symbol;
+                var baseAsset = this.jsonData.symbols[i].baseAsset;
+                var quoteAsset = this.jsonData.symbols[i].quoteAsset;
+                var status = this.jsonData.symbols[i].status;
+                var sql = "INSERT INTO exchangeTickers (symbol,baseAsset,quoteAsset,listingStatus) VALUES (?,?,?,?)";
+                var values = [symbol, baseAsset, quoteAsset, status];
+                pool.query(sql, values, function (err, result) {
+                    if (err) throw err;
+                    console.log("1 record inserted");
+                });
             }
         }
-        console.log(previouseTicker);
-    });
+        console.log(previousTickers);
+        //compare latestTickers,previousTickers. Mark de-listed tickers from Database and remove de-listed symbol from previousTickers
+        const latestTickers = [];
+        for (var i = 0; i < this.jsonData.symbols.length; i++) {
+            if (this.jsonData.symbols[i].contractType === "PERPETUAL") {
+                latestTickers.push(this.jsonData.symbols[i].symbol);
+            }
+        }
+        console.log(latestTickers);
+        return latestTickers;
+    }
+}
+
+//check previousTickers
+const previousTickers = [];
+if (previousTickers == 0) {
+    pool.query(
+        'SELECT symbol,listingStatus FROM cryptodata.exchangeTickers', function (err, results, fields) {
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].listingStatus === "TRADING") {
+                    previousTickers.push(results[i].symbol);
+                }
+            }
+            console.log('load from database.... '+previousTickers);
+        });
+}
+
+//check deListedTicker
+const deListedTicker = [];
+if (deListedTicker == 0) {
+    pool.query(
+        'SELECT symbol,listingStatus FROM cryptodata.exchangeTickers', function (err, results, fields) {
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].listingStatus === "SETTLING") {
+                    deListedTicker.push(results[i].symbol);
+                }
+            }
+            console.log('deListedTicker load from database.... '+deListedTicker);
+        });
+}
+
+const apiExchangeInfo = new apiCollection("https://fapi.binance.com/fapi/v1/exchangeInfo");
+const apiKline = new apiCollection("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=1000");
+const apiInterestRate = new apiCollection("https://fapi.binan");
 
 
-//disconnect
-connection.end();
+setInterval(() => {
+    apiExchangeInfo.getJson().then(
+        () => {
+        apiExchangeInfo.saveExchangeInfo();
+    }
+    )
+},5000);
